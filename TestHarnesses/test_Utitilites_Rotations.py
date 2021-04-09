86e842266d6bbf69ce3daae668d246b36c8d8b3c
taltaffe <taltaffe@ucsc.edu>
2021-04-08 22:10:57 -0700

    lab0 complete pt.1

--- TestHarnesses/test_Utitilites_Rotations.py
+++
@@ -1,8 +0,0 @@
-"""This file is a test harness for the module ece163.Utilities.Rotations.  It is meant to be run from the root directory of the repo with:
-
-python TestHarnesses/test_Utilities_Rotations.py
-
-at which point it will execute various tests on the Rotations module"""
-
-import math
-
