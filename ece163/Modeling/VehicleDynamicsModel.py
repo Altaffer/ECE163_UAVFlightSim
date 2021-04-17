78197e81a4aad272340d68612541b8d28b904d53
taltaffe <taltaffe@ucsc.edu>
2021-04-17 03:21:12 -0700

    lab1 with issues

--- ece163/Modeling/VehicleDynamicsModel.py
+++ ece163/Modeling/VehicleDynamicsModel.py
@@ -10,97 +10,129 @@ def s(angle):
     return math.sin(angle)
 def t(angle):
     return math.tan(angle)
-class VehicleDynamicsModel(dT=0.01):
-    def __init__(self,dT):
+class VehicleDynamicsModel():
+    def __init__(self, dT=VPC.dT):
         self.dT= dT
         self.state = States.vehicleState()
         self.dot = States.vehicleState()
-        #self.derivative.pn =3
+        return
 
     def setVehicleState(self, state):
-        self.state = state
+        #set self.state to the state
+        self.state = self.state
+        return
 
     def getVehicleState(self):
+        #returning back the state
         return self.state
 
     def setVehicleDerivative(self,dot):
-       self.dot = dot
+        #set self.dot to the deritative dot
+        self.dot = dot
+        return
 
     def getVehicleDerivative(self):
+        #get the derivative
         return self.dot
 
     def reset(self):
+        #set state, dot, and dT back to their original point
         self.state = States.vehicleState()
         self.dot = States.vehicleState()
         self.dT = 0.01
+        return
 
     def Update(self, forcesMoments):
-        self.state = self.getVehicleState(forcesMoments)
-        self.state = self.IntegrateState(self.dT, self.state, self.dot,forcesMoments)
-        self.dot = self.derivative(self.state,forcesMoments)
+        #update the time step
+        self.dot = self.derivative(self.state, forcesMoments)
+        self.state = self.IntegrateState(self.dT, self.state, self.dot)
+        return
 
     def Rexp(self, dT, state, dot):
-        wk = [[state.u,state.v,state.w]]
-        wk1 = mm.matrixAdd(wk,[[dT*dot.u,dT*dot.v,dT*dot.w]])
-        w = mm.matrixScalarMultiply(0.5,mm.matrixAdd(wk,wk1))
-        #[[state.p,state.q,state.r]]
-        magw = math.sqrt(w[0][0]**2 + w[0][1]**2 + w[0][2]**2)
-        omegax = [[0,state.r,(-state.q)],[(-state.r),0,state.p],[state.q,(-state.p),0]]
-        omegaxdT = [[0,VPC.dT*state.r,VPC.dT*(-state.q)],[VPC.dT*(-state.r),0,VPC.dT*state.p],[VPC.dT*state.q,VPC.dT*(-state.p),0]]
-        mag_omegaxdT =math.sqrt(omegaxdT[0][0]**2 + omegaxdT[0][1]**2 + omegaxdT[0][2]**2 + omegaxdT[1][0]**2 +
-                                omegaxdT[1][1]**2 + omegaxdT[1][2]**2 + omegaxdT[2][0]**2 + omegaxdT[2][1]**2 +
-                                omegaxdT[2][2] ** 2)
-        if mag_omegaxdT > 0.2:
-            Rexp = (mm.matrixAdd(mm.matrixSubtract([[1,0,0],[0,1,0],[0,0,1]], mm.matrixScalarMultiply(s(magw*dT)/magw,omegax)),
-                                                  mm.matrixScalarMultiply((1-c(magw*dT))/magw**2,mm.matrixMultiply(omegax,omegax))))
+        #trapezoidal approx of p,q,r
+        pp = state.p + (0.5 * dot.p)
+        qq = state.q + (0.5 * dot.q)
+        rr = state.r + (0.5 * dot.q)
+        #taking the magniude of w, trapezoidal
+        magw = math.sqrt(pp**2 + qq**2 + rr**2)
+        omegax = [[0,-rr,(qq)],[(rr),0,-pp],[-qq,(pp),0]]
+        mag_omegaxdT =math.sqrt((2*(pp*dT)**2)+(2*(qq*dT)**2)+(2*(rr*dT)**2))
+        #Using the contraint of the size of mag_w of it being greater than or less than 0.2
+        if mag_omegaxdT >= 0.2:
+            sin = math.sin((magw*dT)/magw)
+            cos = (1-math.cos(magw*dT))/(magw**2)
+            I = ([[1,0,0],[0,1,0],[0,0,1]])
+            Rexp = mm.matrixAdd((mm.matrixSubtract(I, mm.matrixScalarMultiply(sin,omegax))),mm.matrixScalarMultiply(cos,mm.matrixMultiply(omegax,omegax)))
             return Rexp
         else:
-            Rexp2 = (mm.matrixSubtract(mm.matrixSubtract([[1,0,0],[0,1,0],[0,0,1]], mm.matrixScalarMultiply(dT-((dT**(3)*magw**2)/6)+((dT**(5)*magw**4)/120),omegax)),
-                                                  mm.matrixScalarMultiply(((dT**(2)/2)-((dT**(4)*magw**2)/24)+((dT**(6)*magw**4)/720)),mm.matrixMultiply(omegax,omegax))))
+            sin = dT-(((dT**3)*(magw**2))/6)+(((dT**5)*(magw**4))/120)
+            cos = (((dT**2)/2)-(((dT**4)*(magw**2)/24)+(((dT**6)*(magw**4)/720))))
+            I = ([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+            Rexp2 = mm.matrixAdd((mm.matrixSubtract(I, mm.matrixScalarMultiply(sin,omegax))),mm.matrixScalarMultiply(cos,mm.matrixMultiply(omegax,omegax)))
             return Rexp2
 
     def derivative(self, state, forcesMoments):
-        dot = state.VehicleState()
-        z = mm.matrixMultiply(mm.matrixTranspose(Rotations.euler2DCM(state.yaw, state.pitch, state.roll)),
-                                                       [[state.u, state.v, state.w]])
+        dot = States.vehicleState()
+        #derivatives of pn, pe, pd
+        p = mm.matrixMultiply(mm.matrixTranspose((state.R)),
+                              mm.matrixTranspose([[state.u, state.v, state.w]]))
         dot.pn = p[0][0]
-        dot.pe = p[0][1]
-        dot.pd = p[0][2]
-        v = mm.matrixAdd([[(state.r * state.v) - (state.q * state.w),
-                                                 (state.p * state.w) - (state.r * state.u),
-                                                 (state.q * state.u) - (state.p * state.v)]],
-                                               [[forcesMoments.Fx/VPC.mass, forcesMoments.Fy/VPC.mass, forcesMoments.Pz/VPC.mass]])
+        dot.pe = p[1][0]
+        dot.pd = p[2][0]
+        #derivatives of u, v, and w
+        wx_neg = [[0, -state.r, (state.q)], [(state.r), 0, -state.p], [-state.q, (state.p), 0]]
+        f = [[forcesMoments.Fx],[forcesMoments.Fy],[forcesMoments.Fz]]
+        v = mm.matrixSubtract(mm.matrixScalarMultiply(1/VPC.mass,f),mm.matrixMultiply(wx_neg,([[state.u],[state.v],[state.w]])))
         dot.u = v[0][0]
-        dot.v = v[0][0]
-        dot.w = v[0][1]
-        g = mm.matrixMultiply([[1, s(state.roll)*t(state.pitch), c(state.roll)*t(state.pitch)],
-                                                              [0,  c(state.roll), -s(state.roll)],
-                                                              [0, s(state.roll)/c(state.pitch), c(state.roll)/c(state.pitch)]],
-                                                             [[state.p, state.q, state.r]])
-        dot.yaw = g[0][0]
-        dot.pitch = g[0][1]
-        dot.roll = g[0][2]
-        gamma = (VPC.Jxx*VPC.Jyy)-VPC.Jxz**2
-        q = mm.matrixMultiply([[VPC.Jzz/gamma, 0, VPC.Jxz/gamma], [0, 1/VPC.Jyy, 0],
-                                                     [VPC.Jxz/gamma, 0, VPC.Jxx/gamma]],
-                                                    (mm.matrixAdd([[(VPC.Jxz*state.p*state.q)+((VPC.Jyy-VPC.Jzz)*(state.q*state.r)),
-                                                                    VPC.Jxz*(state.r**2-state.p**2)+((state.p*state.r)*(VPC.Jzz-VPC.Jxx)),
-                                                                    ((state.p*state.q)*(VPC.Jxx-VPC.Jyy))-(VPC.Jxz*state.q*state.r)]],
-                                                                  [[forcesMoments.Mx, forcesMoments.My, forcesMoments.Mz]])))
+        dot.v = v[1][0]
+        dot.w = v[2][0]
+
+        #derivatives of yaw,pitch, and roll
+        w = ([[state.p], [state.q], [state.r]])
+        C= [[1, s(state.roll)*t(state.pitch), c(state.roll)*t(state.pitch)],[0,  c(state.roll), -s(state.roll)],
+            [0, s(state.roll)/c(state.pitch), c(state.roll)/c(state.pitch)]]
+        g = mm.matrixMultiply(C,w)
+        dot.yaw = g[2][0]
+        dot.pitch = g[1][0]
+        dot.roll = g[0][0]
+
+        #derivatives of p, p, are r
+        wx = [[0, state.r, (-state.q)], [(-state.r), 0, state.p], [state.q, (-state.p), 0]]
+        M = [[forcesMoments.Mx], [forcesMoments.My], [forcesMoments.Mz]]
+        q = mm.matrixMultiply(VPC.JinvBody, mm.matrixSubtract(M,mm.matrixMultiply(wx,mm.matrixMultiply(VPC.Jbody,w))))
         dot.p = q[0][0]
-        dot.q = q[0][1]
-        dot.r = q[0][2]
-        dot.R = mm.matrixMultiply([[0,state.r,-state.q],[-state.r,0,state.p],[state.q,-state.p,0]], Rotations.euler2DCM(state.yaw,state.pitch,state.roll))
+        dot.q = q[1][0]
+        dot.r = q[2][0]
+
+        #derivative of the rotation matrix R
+        if state.p != 0 and state.q != 0 and state.r != 0:
+            dot.R = mm.matrixMultiply(mm.matrixScalarMultiply(1,wx), state.R)
+        else:
+            dot.R = [[1,0,0],[0,1,0],[0,0,1]]
         return dot
 
     def ForwardEuler(self, dT, state, dot):
-        newState = state + (dT*dot)
-        #tate.pn +(dT*dot.pn)
-        return newState
+        #define all the forward Euler componets of state and dot
+        states = States.vehicleState
+        states.pn = state.pn + (self.dT * dot.pn)
+        states.pe = state.pe + (self.dT * dot.pe)
+        states.pd = state.pd + (self.dT * dot.pd)
+        states.u = state.u + (self.dT * dot.u)
+        states.v = state.v + (self.dT * dot.v)
+        states.w = state.w + (self.dT * dot.w)
+        states.yaw = state.yaw + (self.dT * dot.yaw)
+        states.pitch = state.pitch + (self.dT * dot.pitch)
+        states.roll = state.roll + (self.dT * dot.roll)
+        states.p = state.p + (self.dT * dot.p)
+        states.q = state.q + (self.dT * dot.q)
+        states.r = state.r + (self.dT * dot.r)
+        return states
 
     def IntegrateState(self, dT, state, dot):
-        newState = ForwardEuler(dT, state, dot)
-        newState.R = Rexp(dT,state,dot) * state.dcm
+        #Using forward euler to find the integral of state while using Rexp for R and keeping yaw, pitch, roll, alpha, beta,
+        # Va and chi constant
+        newState = self.ForwardEuler(dT, state, dot)
+        newState.R = mm.matrixMultiply(self.Rexp(dT,state,dot), state.R)
         newState.yaw = state.yaw
         newState.pitch = state.pitch
         newState.roll = state.roll
--- testChapter3.py
+++ testChapter3.py
@@ -13,25 +13,18 @@ import math
 import ece163.Utilities.MatrixMath as mm
 import ece163.Utilities.Rotations as Rotations
 import ece163.Modeling.VehicleGeometry as VG
+import ece163.Containers.States as States
 import ece163.Modeling.VehicleDynamicsModel as VDM
-import ece163.Containers.States
+from ece163.Constants import VehiclePhysicalConstants as VPC
 
 
 """math.isclose doesn't work well for comparing things near 0 unless we 
 use an absolute tolerance, so we make our own isclose:"""
 isclose = lambda  a,b : math.isclose(a, b, abs_tol= 1e-12)
-
 def compareVectors(a, b):
 	"""A quick tool to compare two vectors"""
 	el_close = [isclose(a[i][0], b[i][0]) for i in range(3)]
 	return all(el_close)
-
-#of course, you should test your testing tools too:
-assert(compareVectors([[0], [0], [-1]],[[1e-13], [0], [-1+1e-9]]))
-assert(not compareVectors([[0], [0], [-1]],[[1e-11], [0], [-1]]))
-assert(not compareVectors([[1e8], [0], [-1]],[[1e8+1], [0], [-1]]))
-
-
 
 failed = []
 passed = []
@@ -47,30 +40,224 @@ def evaluateTest(test_name, boolean):
 	return boolean
 
 
-#%% Euler2dcm():
-print("Beginning testing of Rotations.Euler2dcm()")
-
-cur_test = "Euler2dcm yaw test 1"
-#we know that rotating [1,0,0] by 90 degrees about Z should produce [0,-1,0], so
-R = Rotations.euler2DCM(90*math.pi/180, 0, 0)
-orig_vec = [[1],[0],[0]]
-expected_vec = [[0],[-1],[0]]
-actual_vec = mm.matrixMultiply(R, orig_vec)
-if not evaluateTest(cur_test, compareVectors(expected_vec, actual_vec) ):
-	print(f"{expected_vec} != {actual_vec}")
-
 cur_test = "init test 1"
-#we know that rotating [1,0,0] by 90 degrees about Z should produce [0,-1,0], so
-r = VDM.__init__(self,dT)
-expected_vec = States.vehicleState()
-actual_vec = mm.matrixMultiply(R, orig_vec)
-if not evaluateTest(cur_test, compareVectors(expected_vec, actual_vec) ):
-	print(f"{expected_vec} != {actual_vec}")
+#init test for state
+p1 = VDM.VehicleDynamicsModel(0.01)
+if not evaluateTest(cur_test, (p1.state == States.vehicleState()) ):
+	print(f"{p1.state} != {q}")
+cur_test = "init test 2"
+#init test for dot
+p1 = VDM.VehicleDynamicsModel(0.01)
+if not evaluateTest(cur_test, (p1.dot == States.vehicleState()) ):
+	print(f"{p1.dot} != {q}")
+cur_test = "init test 3"
+#init test for dt
+p1 = VDM.VehicleDynamicsModel(0.01)
+if not evaluateTest(cur_test, (p1.dT == VPC.dT) ):
+	print(f"{p1.dot} != {VPC.dT}")
+
 cur_test = "setVehicleState test 1"
+#test for setting the vehicle state
+p1 = VDM.VehicleDynamicsModel(0.01)
+#if not evaluateTest(cur_test, (p1.state == States.vehicleState()) ):
+	#print(f"{p1.dot} != {VPC.dT}")
+
 cur_test = "getVehicleState test 1"
+#test for getting the vehicle state
+p1 = VDM.VehicleDynamicsModel(0.01)
+if not evaluateTest(cur_test, (p1.state == States.vehicleState()) ):
+	print(f"{p1.state} != {States.vehicleState()}")
+
 cur_test = "setVehicleDerivative test 1"
+#test for setting the vehicle Derivative
+
 cur_test = "getVehicleDerivative test 1"
+#test for getting the vehicle Derivative
+p1 = VDM.VehicleDynamicsModel(0.01)
+if not evaluateTest(cur_test, (p1.dot == States.vehicleState()) ):
+	print(f"{p1.dot} != {VPC.dT}")
+
 cur_test = "reset test 1"
+#testing if the reset brings components back to the origin
+
+cur_test = "derivative test 1"
+#testing the derivative of pn
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.pn = 1
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.pn, p2.dot.pn) ):
+	print(f"{p1.dot.pn} != {p2.dot.pn}")
+p2.reset()
+cur_test = "derivative test 2"
+#testing the derivative of pe
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.pe = 2
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.pe, p2.dot.pe) ):
+	print(f"{p1.dot.pe} != {p2.dot.pe}")
+cur_test = "derivative test 3"
+#testing the derivative of pd
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.pd = 3
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.pd, p2.dot.pd) ):
+	print(f"{p1.dot.pd} != {p2.dot.pd}")
+p2.reset()
+cur_test = "derivative test 4"
+#testing the derivative of u
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.u = 4
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.u, p2.dot.u) ):
+	print(f"{p1.dot.u} != {p2.dot.u}")
+cur_test = "derivative test 5"
+#testing the derivative of v
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.v = 5
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.v, p2.dot.v) ):
+	print(f"{p1.dot.v} != {p2.dot.v}")
+p2.reset()
+cur_test = "derivative test 6"
+#testing the derivative of w
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.w = 6
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.w, p2.dot.w) ):
+	print(f"{p1.dot.w} != {p2.dot.w}")
+cur_test = "derivative test 7"
+#testing the derivative of yaw
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.yaw = 7
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.yaw, p2.dot.yaw) ):
+	print(f"{p1.dot.yaw} != {p2.dot.yaw}")
+cur_test = "derivative test 8"
+#testing the derivative of pitch
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.pitch = 8
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.pitch, p2.dot.pitch) ):
+	print(f"{p1.dot.pitch} != {p2.dot.pitch}")
+p2.reset()
+cur_test = "derivative test 9"
+#testing the derivative of roll
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.roll = 9
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.roll, p2.dot.roll) ):
+	print(f"{p1.dot.roll} != {p2.dot.roll}")
+cur_test = "derivative test 10"
+#testing the derivative of p
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.p = 10
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.p, p2.dot.p) ):
+	print(f"{p1.dot.p} != {p2.dot.p}")
+cur_test = "derivative test 11"
+#testing the derivative of q
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.q = 11
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.q, p2.dot.q) ):
+	print(f"{p1.dot.q} != {p2.dot.q}")
+p2.reset()
+cur_test = "derivative test 12"
+#testing the derivative of r
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.r = 12
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.r, p2.dot.r) ):
+	print(f"{p1.dot.r} != {p2.dot.r}")
+cur_test = "derivative test 13"
+#testing the derivative of R
+p1 = VDM.VehicleDynamicsModel(0.01)
+p2 = VDM.VehicleDynamicsModel(0.01)
+p2.dot.R = 12
+p1.dot = p2.getVehicleDerivative()
+if not evaluateTest(cur_test, isclose(p1.dot.R, p2.dot.R) ):
+	print(f"{p1.dot.R} != {p2.dot.R}")
+
+cur_test = "Rexp test 1"
+p1 = VDM.VehicleDynamicsModel()
+p1.state.p = 1
+p1.dot.p = 2
+p1.state.q = 3
+p1.dot.q = 4
+p1.state.r=5
+p1.dot.r=6
+Rexp_r = p1.Rexp(p1.dT, p1.state, p1.dot)
+expec = [[0.9963024056253, 0.0704087104055, -0.049235480468299994],[-0.06940936057449999, 0.99735172294785, 0.021722872344249997],[0.050634570231699996, -0.01822514793575, 0.99855094274505]]
+if not evaluateTest(cur_test, (Rexp_r==expec)):
+	print(f"{Rexp_r} != {expec}")
+
+cur_test = "IntegrateState test 1"
+statex = States.vehicleState()
+p1 = VDM.VehicleDynamicsModel()
+p2 = VDM.VehicleDynamicsModel()
+dT= VPC.dT
+p2.state.pn = 2
+p2.state.yaw = 45
+p2.state.p = 5
+p2.state.u = 3
+expec_pn = 2
+statex = p2.IntegrateState(dT, p2.state, p2.dot)
+if not evaluateTest(cur_test, (statex.pn == expec_pn) ):
+	print(f"{statex.pn} != {expec_pn}")
+cur_test = "IntegrateState test 2"
+statex = States.vehicleState()
+p1 = VDM.VehicleDynamicsModel()
+p2 = VDM.VehicleDynamicsModel()
+dT= VPC.dT
+p2.state.pe = 2
+p2.state.pitch = 45
+p2.state.q = 5
+p2.state.v = 3
+expec_v = 3
+statex = p2.IntegrateState(dT, p2.state, p2.dot)
+if not evaluateTest(cur_test, (statex.v == expec_v) ):
+	print(f"{statex.v} != {expec_v}")
+cur_test = "IntegrateState test 3"
+statex = States.vehicleState()
+p1 = VDM.VehicleDynamicsModel()
+p2 = VDM.VehicleDynamicsModel()
+dT= VPC.dT
+p2.state.pd = 2
+p2.state.roll = 45
+p2.state.r = 5
+p2.state.w = 3
+expec_roll = 45
+statex = p2.IntegrateState(dT, p2.state, p2.dot)
+if not evaluateTest(cur_test, (statex.roll == expec_roll) ):
+	print(f"{statex.roll} != {expec_roll}")
+cur_test = "ForwardEuler test 1"
+statex = States.vehicleState()
+stated = States.vehicleState()
+statex.pn = 1
+stated.pn = 1
+statex.v = 1
+stated.v = 1
+statex.r = 1
+stated.r=1
+p1 = VDM.VehicleDynamicsModel()
+p2=p1.getVehicleDerivative()
+fEuler =p1.ForwardEuler(0.01,statex, stated)
+a = [p2.pn,p2.pe,p2.pd,p2.u,p2.v,p2.w,p2.p,p2.q,p2.r]
+expec = [0,0,0,0,0,0,0,0,0]
+if not evaluateTest(cur_test, (expec == a) ):
+	print(f"{expec} != {a}")
 
 #%% Print results:
 
